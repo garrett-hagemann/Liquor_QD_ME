dca6f453c6c14ec885bbd919844ee7c7181f70dd
Garrett Hagemann <gh8728@snuffles.la.utexas.edu>
2016-09-06 11:58:22 -0500

    Added change of variables for distribution parameters. Added code to interpolate p_star over a big grid, but not impletment it in FOCs yet

--- indirect_est.jl
+++ indirect_est.jl
@@ -1,4 +1,20 @@
-using DataArrays, DataFrames, ForwardDiff, NLsolve, Roots, NLopt, Distributions
+using DataArrays, DataFrames, ForwardDiff, NLsolve, Roots, Distributions, Optim, Interpolations
+
+#= goofy conversion magic =#
+import Base.convert
+function convert(t::Type{Float64},x::Array{Float64,1})
+	return convert(Float64,x[1])
+end
+
+import Base.isless
+function isless(x::Array{Float64,1},y::Float64)
+	return isless(x[1],y)
+end
+function isless(x::Array{Float64,1},y::Array{Float64,1})
+	return isless(x[1],y[1])
+end
+
+
 
 df = readtable("../../demand_estimation/berry_logit/berry_logit.csv")
 char_list = [:price, :d_gin, :d_vod, :d_rum, :d_sch, :d_brb, :d_whs, :holiday]
@@ -38,9 +54,9 @@ function sparse_int(f::Function, a::Number, b::Number)
 	b = upperbound of integration
 
 	=#
-	 weights = big([.0001816, .00063259999999999998, .0012895000000000001, .0021088000000000001, .0030577999999999998, .0041114999999999997, .0052490999999999996, .0064519, .0077034, .0089893000000000004, .0102971, .0116157, .0129348, .0142449, .0155368, .016801900000000002, .018032200000000002, .019219900000000002, .020357799999999999, .021439, .022457299999999999, .023406799999999998, .0242822, .0250786, .025791600000000001, .0264175, .0269527, .027394600000000002, .0277407, .027989199999999999, .028138799999999999, .0281888, .028138799999999999, .027989199999999999, .0277407, .027394600000000002, .0269527, .0264175, .025791600000000001, .0250786, .0242822, .023406799999999998, .022457299999999999, .021439, .020357799999999999, .019219900000000002, .018032200000000002, .016801900000000002, .0155368, .0142449, .0129348, .0116157, .0102971, .0089893000000000004, .0077034, .0064519, .0052490999999999996, .0041114999999999997, .0030577999999999998, .0021088000000000001, .0012895000000000001, .00063259999999999998, .0001816])
+	 weights = [.0001816, .00063259999999999998, .0012895000000000001, .0021088000000000001, .0030577999999999998, .0041114999999999997, .0052490999999999996, .0064519, .0077034, .0089893000000000004, .0102971, .0116157, .0129348, .0142449, .0155368, .016801900000000002, .018032200000000002, .019219900000000002, .020357799999999999, .021439, .022457299999999999, .023406799999999998, .0242822, .0250786, .025791600000000001, .0264175, .0269527, .027394600000000002, .0277407, .027989199999999999, .028138799999999999, .0281888, .028138799999999999, .027989199999999999, .0277407, .027394600000000002, .0269527, .0264175, .025791600000000001, .0250786, .0242822, .023406799999999998, .022457299999999999, .021439, .020357799999999999, .019219900000000002, .018032200000000002, .016801900000000002, .0155368, .0142449, .0129348, .0116157, .0102971, .0089893000000000004, .0077034, .0064519, .0052490999999999996, .0041114999999999997, .0030577999999999998, .0021088000000000001, .0012895000000000001, .00063259999999999998, .0001816]
 
-	nodes = big([.00006349999999999412, .00045090000000003183, .0013969000000000342, .0030839999999999756, .0056576000000000404, .009234399999999976, .013908600000000049, .01975439999999995, .02682859999999998, .035172599999999998, .044814400000000032, .055770399999999998, .068046000000000051, .08163699999999996, .096529700000000052, .11270170000000002, .13012199999999996, .14875190000000005, .16854519999999995, .18944850000000002, .21140210000000004, .23434010000000005, .25819099999999995, .28287810000000002, .30832029999999999, .33443230000000002, .36112509999999998, .3883067, .41588230000000004, .44375549999999997, .47182789999999997, .5, .52817210000000003, .55624450000000003, .58411769999999996, .6116933, .63887490000000002, .66556769999999998, .69167970000000001, .71712189999999998, .74180900000000005, .76565989999999995, .78859789999999996, .81055149999999998, .83145480000000005, .85124809999999995, .86987800000000004, .88729829999999998, .90347029999999995, .91836300000000004, .93195399999999995, .9442296, .95518559999999997, .9648274, .97317140000000002, .98024560000000005, .98609139999999995, .99076560000000002, .99434239999999996, .99691600000000002, .99860309999999997, .99954909999999997, .99993650000000001])
+	nodes = [.00006349999999999412, .00045090000000003183, .0013969000000000342, .0030839999999999756, .0056576000000000404, .009234399999999976, .013908600000000049, .01975439999999995, .02682859999999998, .035172599999999998, .044814400000000032, .055770399999999998, .068046000000000051, .08163699999999996, .096529700000000052, .11270170000000002, .13012199999999996, .14875190000000005, .16854519999999995, .18944850000000002, .21140210000000004, .23434010000000005, .25819099999999995, .28287810000000002, .30832029999999999, .33443230000000002, .36112509999999998, .3883067, .41588230000000004, .44375549999999997, .47182789999999997, .5, .52817210000000003, .55624450000000003, .58411769999999996, .6116933, .63887490000000002, .66556769999999998, .69167970000000001, .71712189999999998, .74180900000000005, .76565989999999995, .78859789999999996, .81055149999999998, .83145480000000005, .85124809999999995, .86987800000000004, .88729829999999998, .90347029999999995, .91836300000000004, .93195399999999995, .9442296, .95518559999999997, .9648274, .97317140000000002, .98024560000000005, .98609139999999995, .99076560000000002, .99434239999999996, .99691600000000002, .99860309999999997, .99954909999999997, .99993650000000001]
 
 	f_evals = [f((b-a)*u + a) for u in nodes]
 	return dot(f_evals, weights)*(b-a)
@@ -61,14 +77,8 @@ for market in markets
 	products = [650]
 	
 	for product in products
-		# defning params for wholesaler problem
-		c = 10 # marginal cost for wholesaler
-		max_mc = 20 # max MC for retailer. Scales type parameter
-		a = 1 # first param for Beta distribution
-		b = 1 # second param for Beta distribution
-		
+		tic()
 		println("Working with Market: $market, Product: $product")
-
 		# defining selection boolians so that we can select appropriate products/markets
 		prod_bool = ((df[:product] .== product) & (df[:mkt] .== market))
 		other_bool = ((df[:product] .!= product) & (df[:mkt] .== market))
@@ -116,9 +126,6 @@ for market in markets
 				intercept = y_intersect - obs_rhos[k]*obs_cutoff_q[k] # where does current unit price interesct vertical axis?
 				push!(obs_ff,intercept) # append to list obs_ff
 			end
-			println(obs_rhos)
-			println(obs_cutoff_q)
-			println(obs_ff)
 
 			#Defining share function which is ONLY a function of price
 			function share(p)
@@ -133,12 +140,12 @@ for market in markets
 				
 				s = num/denom	
 
-				return s
+				return s[1] # dealing with weird type promotion stuff
 			end
 
 			#Derivatives of the share fuction
-			d_share(p) = ForwardDiff.derivative(share, p)
-			dd_share(p) = ForwardDiff.derivative(d_share,p)
+			d_share(p) = ForwardDiff.derivative(share, p[1]) #p should be scalar. Optimization routine returns a 1 element array at some point
+			dd_share(p) = ForwardDiff.derivative(d_share,p[1])
 
 			function price_sched_calc(params,N)
 				# params are the params of the wholesaler's problem we're trying to
@@ -149,30 +156,31 @@ for market in markets
 
 				c = params[1] # marginal cost for wholesaler
 				max_mc = params[2] # max MC for retailer. Scales type parameter
-				a = params[3] # first param for Beta distribution
-				b = params[4] # second param for Beta distribution
+				a = exp(params[3]) # first param for Beta distribution
+				b = exp(params[4]) # second param for Beta distribution
 				est_pdf(x) = pdf(Beta(a,b),x)
 
 				#Defining p-star function. Requires solving NL equation
 				function p_star(rho::Number,lambda::Number)
-					g(p) =  (p - rho - lambda*max_mc)*d_share(p) + share(p)
-					res = fzero(g,(rho+lambda*max_mc)) # upper bound here can be finicky
-					return res
+					function g!(p,gvec)
+						gvec[1] =  (p - rho - lambda*max_mc)*d_share(p) + share(p)
+					end
+					
+					res = nlsolve(g!,[rho+lambda*max_mc]) 
+					return res.zero[1]
 				end
 
-				#= Derivative of the p_star function. Need to use central difference approx
-				# because auto diff doesn't with with fzero
-				eps = 1e-16
-				#function d_pstar_d_rho(rho,lambda)
-					res = (p_star(rho + eps,lambda) - p_star(rho - eps,lambda)) / (2*eps)
-					return res
-				end=#
-
+				# Derivative of the p_star function. Need to use central difference approx
 				function d_pstar_d_rho(rho,lambda)
 					res = d_share(p_star(rho,lambda)) / (dd_share(p_star(rho,lambda))*(p_star(rho,lambda) - rho - lambda*max_mc) + 2*d_share(p_star(rho,lambda)))
-					return res
+					return res[1]
 				end
+				
 
+				#=Interpolationg p_star at a bunch of points to help speed up calculation =#
+				p_star_grid = [p_star(i,j) for i = 1:100, j = 1:100]
+				p_star_grid = convert(Array{Float64,2},p_star_grid)
+				p_star_itp = interpolate(p_star_grid, BSpline(Quadratic(Reflect())), OnGrid())
 				
 				##### CODE THAT USES NLSOLVE #####
 
@@ -181,27 +189,24 @@ for market in markets
 				# NOTE: Can refomulate as constrained minimization if this doesn't work
 
 				function wfocs!(theta::Vector, wfocs_vec)
-					tic()
 					nfocs = length(theta) # number of FOCs (last param is wholesaler cost)
 					n = round(Int,(nfocs + 1)/2) # figuring out how many parts the price schedule has
 					#println("Solving for $n part price schedule")
 									
 					lambda_vec = [lambda_lb ; theta[n+1:end] ; lambda_ub]
 					rho_vec = theta[1:n]
-					
 					for i in 1:length(rho_vec)
 						f(l) =  ((rho_vec[i] - c)*d_share(p_star(rho_vec[i],l))*d_pstar_d_rho(rho_vec[i],l) + share(p_star(rho_vec[i],l)))*est_pdf(l)
-						wfocs_vec[i] = quadgk(f,big(lambda_vec[i]), big(lambda_vec[i+1]); abstol= 1e-16)[1]
+					#	wfocs_vec[i] = quadgk(f,big(lambda_vec[i]), big(lambda_vec[i+1]); abstol= 1e-16)[1]
+						wfocs_vec[i] = sparse_int(f,lambda_vec[i], lambda_vec[i+1])
 					end
-
 					for i in 1:(length(lambda_vec)-2) # indexing is inclusive so x:x = x for any x
-						wfocs_vec[i + length(rho_vec)] = share(p_star(rho_vec[i],lambda_vec[i+1]))*(p_star(rho_vec[i],lambda_vec[i+1]) - lambda_vec[i+1]*max_mc - c) - share(p_star(rho_vec[i+1], lambda_vec[i+1]))*(p_star(rho_vec[i+1],lambda_vec[i+1]) - rho_vec[i+1] - lambda_vec[i+1]*max_mc + rho_vec[i] - c)
+					wfocs_vec[i + length(rho_vec)] = share(p_star(rho_vec[i],lambda_vec[i+1]))*(p_star(rho_vec[i],lambda_vec[i+1]) - lambda_vec[i+1]*max_mc - c) - share(p_star(rho_vec[i+1], lambda_vec[i+1]))*(p_star(rho_vec[i+1],lambda_vec[i+1]) - rho_vec[i+1] - lambda_vec[i+1]*max_mc + rho_vec[i] - c)
 					end
-					toc()
 				end 
 				
 				x0 = ones(2*N-1)/2
-				solution = nlsolve(wfocs!, x0,method=:trust_region, show_trace = true, ftol = 1e-12)
+				solution = nlsolve(wfocs!, x0,method=:trust_region, show_trace = false, ftol = 1e-8)
 				est_rhos = solution.zero[1:N] # need to return this. These are the prices for the price schedule
 				est_lambdas = [lambda_lb; solution.zero[N+1:end]; lambda_ub] # remaining parameters are lambda cutoffs
 				
@@ -213,13 +218,30 @@ for market in markets
 				end
 				return (est_rhos,est_ff,est_lambdas)
 			end
-			println(price_sched_calc([7,13,1,1],2))
-
-			#optimizer code goes here
-	
+			
+			function obj_func(omega::Vector, N::Int, W::Matrix)
+				rho_hat,ff_hat,lambda_hat = price_sched_calc(omega,N)
+				vec = [(rho_hat - obs_rhos) ; (ff_hat - obs_ff)]'
+				res = vec*W*vec'
+				println(res)
+				return res[1]
+			end
+		
+			N = 4
+			W = eye(2*N)
+			x0 = [7.0,13.0,1.0,1.0]
+			g(x) = obj_func(x,N,W)
+			#println(g(x0))
+			optim_res = optimize(g,x0,NelderMead(),OptimizationOptions(show_every = true))
+			println(optim_res)
+			min_X = Optim.minimizer(optim_res)
+			println(min_X)
+			println(price_sched_calc(min_X,N))
+			println(obs_rhos)
+			println(obs_ff)
 		else
 			println("Product has no matching price data.")
 		end
-
+		toc()
 	end
 end

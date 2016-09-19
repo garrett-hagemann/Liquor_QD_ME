9586926aec7d83eaee2b33fbf08da1e5c457ff55
Garrett Hagemann <gh8728@snuffles.la.utexas.edu>
2016-09-19 10:27:07 -0500

    Added Jacobian for price schedule calculation. This results in ~ 8 secs to evaluate the objective function using the EXACT derivatives for the p_star funciton and the exact calculation for the p_star solution. Optimizer currently using Simulated Annealing with 1000 iterations (not nearly enough but I don't have 3 days to wait for it to converge)

--- indirect_est.jl
+++ indirect_est.jl
@@ -14,6 +14,7 @@ function isless(x::Array{Float64,1},y::Array{Float64,1})
 	return isless(x[1],y[1])
 end
 
+srand(69510606) #seeding random number gen
 
 
 df = readtable("../../demand_estimation/berry_logit/berry_logit.csv")
@@ -54,14 +55,13 @@ function sparse_int(f::Function, a::Number, b::Number)
 	b = upperbound of integration
 
 	=#
-	 weights = big([.0001816, .00063259999999999998, .0012895000000000001, .0021088000000000001, .0030577999999999998, .0041114999999999997, .0052490999999999996, .0064519, .0077034, .0089893000000000004, .0102971, .0116157, .0129348, .0142449, .0155368, .016801900000000002, .018032200000000002, .019219900000000002, .020357799999999999, .021439, .022457299999999999, .023406799999999998, .0242822, .0250786, .025791600000000001, .0264175, .0269527, .027394600000000002, .0277407, .027989199999999999, .028138799999999999, .0281888, .028138799999999999, .027989199999999999, .0277407, .027394600000000002, .0269527, .0264175, .025791600000000001, .0250786, .0242822, .023406799999999998, .022457299999999999, .021439, .020357799999999999, .019219900000000002, .018032200000000002, .016801900000000002, .0155368, .0142449, .0129348, .0116157, .0102971, .0089893000000000004, .0077034, .0064519, .0052490999999999996, .0041114999999999997, .0030577999999999998, .0021088000000000001, .0012895000000000001, .00063259999999999998, .0001816])
+	 weights = [.0001816, .00063259999999999998, .0012895000000000001, .0021088000000000001, .0030577999999999998, .0041114999999999997, .0052490999999999996, .0064519, .0077034, .0089893000000000004, .0102971, .0116157, .0129348, .0142449, .0155368, .016801900000000002, .018032200000000002, .019219900000000002, .020357799999999999, .021439, .022457299999999999, .023406799999999998, .0242822, .0250786, .025791600000000001, .0264175, .0269527, .027394600000000002, .0277407, .027989199999999999, .028138799999999999, .0281888, .028138799999999999, .027989199999999999, .0277407, .027394600000000002, .0269527, .0264175, .025791600000000001, .0250786, .0242822, .023406799999999998, .022457299999999999, .021439, .020357799999999999, .019219900000000002, .018032200000000002, .016801900000000002, .0155368, .0142449, .0129348, .0116157, .0102971, .0089893000000000004, .0077034, .0064519, .0052490999999999996, .0041114999999999997, .0030577999999999998, .0021088000000000001, .0012895000000000001, .00063259999999999998, .0001816]
 
-	nodes = big([.00006349999999999412, .00045090000000003183, .0013969000000000342, .0030839999999999756, .0056576000000000404, .009234399999999976, .013908600000000049, .01975439999999995, .02682859999999998, .035172599999999998, .044814400000000032, .055770399999999998, .068046000000000051, .08163699999999996, .096529700000000052, .11270170000000002, .13012199999999996, .14875190000000005, .16854519999999995, .18944850000000002, .21140210000000004, .23434010000000005, .25819099999999995, .28287810000000002, .30832029999999999, .33443230000000002, .36112509999999998, .3883067, .41588230000000004, .44375549999999997, .47182789999999997, .5, .52817210000000003, .55624450000000003, .58411769999999996, .6116933, .63887490000000002, .66556769999999998, .69167970000000001, .71712189999999998, .74180900000000005, .76565989999999995, .78859789999999996, .81055149999999998, .83145480000000005, .85124809999999995, .86987800000000004, .88729829999999998, .90347029999999995, .91836300000000004, .93195399999999995, .9442296, .95518559999999997, .9648274, .97317140000000002, .98024560000000005, .98609139999999995, .99076560000000002, .99434239999999996, .99691600000000002, .99860309999999997, .99954909999999997, .99993650000000001])
+	nodes = [.00006349999999999412, .00045090000000003183, .0013969000000000342, .0030839999999999756, .0056576000000000404, .009234399999999976, .013908600000000049, .01975439999999995, .02682859999999998, .035172599999999998, .044814400000000032, .055770399999999998, .068046000000000051, .08163699999999996, .096529700000000052, .11270170000000002, .13012199999999996, .14875190000000005, .16854519999999995, .18944850000000002, .21140210000000004, .23434010000000005, .25819099999999995, .28287810000000002, .30832029999999999, .33443230000000002, .36112509999999998, .3883067, .41588230000000004, .44375549999999997, .47182789999999997, .5, .52817210000000003, .55624450000000003, .58411769999999996, .6116933, .63887490000000002, .66556769999999998, .69167970000000001, .71712189999999998, .74180900000000005, .76565989999999995, .78859789999999996, .81055149999999998, .83145480000000005, .85124809999999995, .86987800000000004, .88729829999999998, .90347029999999995, .91836300000000004, .93195399999999995, .9442296, .95518559999999997, .9648274, .97317140000000002, .98024560000000005, .98609139999999995, .99076560000000002, .99434239999999996, .99691600000000002, .99860309999999997, .99954909999999997, .99993650000000001]
 
 	f_evals = [f((b-a)*u + a) for u in nodes]
 	return dot(f_evals, weights)*(b-a)
 end
-
 
 # defining limits of type distribution. Should be consistent with whatever PDF is used
 lambda_lb = 0
@@ -148,24 +148,40 @@ for market in markets
 			dd_share(p) = ForwardDiff.derivative(d_share,p[1])
 			ddd_share(p) = ForwardDiff.derivative(dd_share,p[1])
 
-			function p_star(rho::Number,l::Number)
+			function p_star(rho,l)
 				function g!(p,gvec)
 					gvec[1] =  (p - rho - l)*d_share(p) + share(p)
 				end
 				function gj!(p, gjvec)
-					gjvec[1] = (p - rho - l)*dd_share(p) + 2.0*share(p)
+					gjvec[1] = (p - rho - l)*dd_share(p) + 2.0*d_share(p)
 				end
 				
-				res = nlsolve(g!,[rho+l]) 
+				res = nlsolve(g!,gj!,[rho+l],show_trace = false, method = :trust_region, extended_trace = false) 
 				return res.zero[1]
 			end
-
 
 			#=Interpolationg p_star at a bunch of points to help speed up calculation =#
 			p_star_grid = [p_star(i,j) for i = 1.0:100.0, j = 1.0:100.0]
 			p_star_grid = convert(Array{Float64,2},p_star_grid)
 			p_star_itp = interpolate(p_star_grid, BSpline(Quadratic(Reflect())), OnGrid())
-			
+			function d_pstar_d_rho(rho,lambda) # note that this is the same as d_pstar_d_lambda
+				u = p_star(rho,lambda)
+				res = d_share(u) / (dd_share(u)*(u - rho - lambda) + 2*d_share(u))
+				return res[1]
+			end
+			function d_pstar_d_lambda(rho,lambda) # see above
+				return d_pstar_d_rho(rho,lambda)
+			end
+			function d2_pstar_d2_rho(rho,lambda)
+				u = p_star(rho,lambda)
+				num1 = (dd_share(u)*(u - rho - lambda) + 2*d_share(u))*dd_share(u)*d_pstar_d_rho(rho,lambda)
+				num2 = d_share(u)*(dd_share(u)*(d_pstar_d_rho(rho,lambda) - 1) + ((u - rho - lambda)*ddd_share(u) + 2*dd_share(u))*d_pstar_d_rho(rho,lambda))
+				den = dd_share(u)*(u - rho - lambda) + 2*d_share(u)
+				res = (num1 - num2)/(den^2)
+D
+				return res
+			end
+
 			function price_sched_calc(params,N)
 				# params are the params of the wholesaler's problem we're trying to
 				# estimate. 
@@ -195,7 +211,7 @@ for market in markets
 				p_star_grid = [p_star(i,j) for i = 1.0:100.0, j = 1.0:100.0]
 				p_star_grid = convert(Array{Float64,2},p_star_grid)
 				p_star_itp = interpolate(p_star_grid, BSpline(Quadratic(Reflect())), OnGrid())
-				=#
+				
 				# Derivative of the p_star function. Need to use central difference approx
 				function d_pstar_d_rho(rho,lambda) # note that this is the same as d_pstar_d_lambda
 					res = d_share(p_star_itp[rho,lambda]) / (dd_share(p_star_itp[rho,lambda])*(p_star_itp[rho,lambda] - rho - lambda) + 2*d_share(p_star_itp[rho,lambda]))
@@ -211,7 +227,7 @@ for market in markets
 					res = (num1 - num2)/(den^2)
 					return res
 				end
-				
+				=#	
 				
 				##### CODE THAT USES NLSOLVE #####
 
@@ -227,12 +243,12 @@ for market in markets
 					lambda_vec = [lambda_lb ; theta[n+1:end] ; lambda_ub]
 					rho_vec = theta[1:n]
 					for i in 1:length(rho_vec)
-						f(l) =  ((rho_vec[i] - c)*d_share(p_star_itp[rho_vec[i],l])*d_pstar_d_rho(rho_vec[i],l) + share(p_star_itp[rho_vec[i],l]))*est_pdf(l)
-					#	wfocs_vec[i] = quadgk(f,big(lambda_vec[i]), big(lambda_vec[i+1]); abstol= 1e-16)[1]
+						f(l) =  ((rho_vec[i] - c)*d_share(p_star(rho_vec[i],l))*d_pstar_d_rho(rho_vec[i],l) + share(p_star(rho_vec[i],l)))*est_pdf(l)
+						#wfocs_vec[i] = quadgk(f,lambda_vec[i], lambda_vec[i+1]; abstol= 1e-16)[1]
 						wfocs_vec[i] = sparse_int(f,lambda_vec[i], lambda_vec[i+1])
 					end
 					for i in 1:(length(lambda_vec)-2) # indexing is inclusive so x:x = x for any x
-					wfocs_vec[i + length(rho_vec)] = share(p_star_itp[rho_vec[i],lambda_vec[i+1]])*(p_star_itp[rho_vec[i],lambda_vec[i+1]] - lambda_vec[i+1] - c) - share(p_star_itp[rho_vec[i+1], lambda_vec[i+1]])*(p_star_itp[rho_vec[i+1],lambda_vec[i+1]] - rho_vec[i+1] - lambda_vec[i+1] + rho_vec[i] - c)
+					wfocs_vec[i + length(rho_vec)] = share(p_star(rho_vec[i],lambda_vec[i+1]))*(p_star(rho_vec[i],lambda_vec[i+1]) - lambda_vec[i+1] - c) - share(p_star(rho_vec[i+1], lambda_vec[i+1]))*(p_star(rho_vec[i+1],lambda_vec[i+1]) - rho_vec[i+1] - lambda_vec[i+1] + rho_vec[i] - c)
 					end
 					print(".") # print . for each eval of wfocs
 				end
@@ -245,8 +261,8 @@ for market in markets
 					for k in 1:length(rho_vec)
 						for j in 1:length(rho_vec)
 							if j==k
-								f(l) = ((rho_vec[k] - c)*(d_share(p_star_itp[rho_vec[k],l])*d2_pstar_d2_rho(rho_vec[k],l) + d_pstar_d_rho(rho_vec[k], l)^2*dd_share(p_star_itp[rho_vec[k],l])) + d_share(p_star_itp[rho_vec[k],l])*d_pstar_d_rho(rho_vec[k],l)*2)*est_pdf(l)
-								wfocs_jac_vec[k,j] = sparse_int(f,lambda_vec[k],lambda_vec[k+1])
+								f(l) = ((rho_vec[k] - c)*(d_share(p_star(rho_vec[k],l))*d2_pstar_d2_rho(rho_vec[k],l) + (d_pstar_d_rho(rho_vec[k], l)^2)*dd_share(p_star(rho_vec[k],l))) + d_share(p_star(rho_vec[k],l))*d_pstar_d_rho(rho_vec[k],l)*2)*est_pdf(l)
+								wfocs_jac_vec[k,j] = (sparse_int(f,lambda_vec[k],lambda_vec[k+1]))
 							else
 								wfocs_jac_vec[k,j] = 0
 							end
@@ -256,9 +272,11 @@ for market in markets
 					for k in 1:length(rho_vec)
 						for j in 2:(length(lambda_vec)-1) # different range to line up j,k correctly
 							if j == k
-								wfocs_jac_vec[k,j+(length(rho_vec)-1)] = -((rho_vec[k] - c)*d_share(p_star_itp[rho_vec[k],lambda_vec[j]])*d_pstar_d_lambda(rho_vec[k],lambda_vec[k]) + share(p_star_itp[rho_vec[k],lambda_vec[j]]))*est_pdf(lambda_vec[j])
+								u = p_star(rho_vec[k],lambda_vec[j])
+								wfocs_jac_vec[k,j+(length(rho_vec)-1)] = (-((rho_vec[k] - c)*d_share(u)*d_pstar_d_lambda(rho_vec[k],lambda_vec[k]) + share(u))*est_pdf(lambda_vec[j]))
 							elseif j == k+1
-								wfocs_jac_vec[k,j+(length(rho_vec)-1)] = ((rho_vec[k] - c)*d_share(p_star_itp[rho_vec[k],lambda_vec[j]])*d_pstar_d_lambda(rho_vec[k],lambda_vec[k]) + share(p_star_itp[rho_vec[k],lambda_vec[j]]))*est_pdf(lambda_vec[j])
+								u = p_star(rho_vec[k],lambda_vec[j])
+								wfocs_jac_vec[k,j+(length(rho_vec)-1)] = (((rho_vec[k] - c)*d_share(u)*d_pstar_d_lambda(rho_vec[k],lambda_vec[k]) + share(u))*est_pdf(lambda_vec[j]))
 							else
 								wfocs_jac_vec[k,j+(length(rho_vec)-1)] = 0
 							end
@@ -268,9 +286,11 @@ for market in markets
 					for k in 2:(length(lambda_vec)-1)
 						for j in 1:length(rho_vec)
 							if j == k-1
-								wfocs_jac_vec[k+(length(rho_vec)-1),j]= share(p_star_itp[rho_vec[j],lambda_vec[k]])*d_pstar_d_rho(rho_vec[j],lambda_vec[k]) + (p_star_itp[rho_vec[j],lambda_vec[k]] - lambda_vec[k] - c)*d_share(p_star_itp[rho_vec[j],lambda_vec[k]])*d_pstar_d_rho(rho_vec[j],lambda_vec[k]) - share(p_star_itp[rho_vec[j+1],lambda_vec[k]])
+								u = p_star(rho_vec[j],lambda_vec[k])
+								wfocs_jac_vec[k+(length(rho_vec)-1),j]= (share(u)*d_pstar_d_rho(rho_vec[j],lambda_vec[k]) + (u - lambda_vec[k] - c)*d_share(u)*d_pstar_d_rho(rho_vec[j],lambda_vec[k]) - share(p_star(rho_vec[j+1],lambda_vec[k])))
 							elseif j == k
-								wfocs_jac_vec[k+(length(rho_vec)-1),j] = share(p_star_itp[rho_vec[j],lambda_vec[k]])*(d_pstar_d_rho(rho_vec[j],lambda_vec[k]) - 1) + (p_star_itp[rho_vec[j],lambda_vec[k]] - rho_vec[j] - lambda_vec[k] + rho_vec[j-1] - c)*d_share(p_star_itp[rho_vec[j],lambda_vec[k]])*d_pstar_d_rho(rho_vec[j],lambda_vec[k])
+								u = p_star(rho_vec[j],lambda_vec[k])
+								wfocs_jac_vec[k+(length(rho_vec)-1),j] = -(share(u)*(d_pstar_d_rho(rho_vec[j],lambda_vec[k]) - 1.0) + (u - rho_vec[j] - lambda_vec[k] + rho_vec[j-1] - c)*d_share(u)*d_pstar_d_rho(rho_vec[j],lambda_vec[k]))
 							else
 								wfocs_jac_vec[k+(length(rho_vec)-1),j] = 0
 							end
@@ -280,16 +300,19 @@ for market in markets
 					for k in 2:(length(lambda_vec)-1)
 						for j in 2:(length(lambda_vec)-1)
 							if j == k
-								wfocs_jac_vec[k+(length(rho_vec)-1),j+(length(rho_vec)-1)] = share(p_star_itp[rho_vec[k-1],lambda_vec[k]])*(d_pstar_d_lambda(rho_vec[k-1],lambda_vec[k]) - 1) + (p_star_itp[rho_vec[k-1],lambda_vec[k]] - lambda_vec[k] - c)*d_share(p_star_itp[rho_vec[k-1],lambda_vec[k]])d_pstar_d_lambda(rho_vec[k-1],lambda_vec[k]) - (share(p_star_itp[rho_vec[k],lambda_vec[k]])*(d_pstar_d_lambda(rho_vec[k],lambda_vec[k]) - 1) + (p_star_itp[rho_vec[k], lambda_vec[k]] - rho_vec[k] - lambda_vec[k] + rho_vec[k-1] - c)*d_share(p_star_itp[rho_vec[k],lambda_vec[k]])*d_pstar_d_lambda(rho_vec[k],lambda_vec[k]))	
+								u1 = p_star(rho_vec[k-1],lambda_vec[k])
+								u2 = p_star(rho_vec[k],lambda_vec[k])
+								wfocs_jac_vec[k+(length(rho_vec)-1),j+(length(rho_vec)-1)] = (share(u1)*(d_pstar_d_lambda(rho_vec[k-1],lambda_vec[k]) - 1) + (u1 - lambda_vec[k] - c)*d_share(u1)*d_pstar_d_lambda(rho_vec[k-1],lambda_vec[k]) - (share(u2)*(d_pstar_d_lambda(rho_vec[k],lambda_vec[k]) - 1) + (u2 - rho_vec[k] - lambda_vec[k] + rho_vec[k-1] - c)*d_share(u2)*d_pstar_d_lambda(rho_vec[k],lambda_vec[k])))
 							else
 								wfocs_jac_vec[k+(length(rho_vec)-1),j+(length(rho_vec)-1)] = 0
 							end
 						end
 					end 
 				end 
-				
-				x0 = ones(2*N-1)/2
-				solution = nlsolve(wfocs!, wfocs_jac!,x0,method=:trust_region, show_trace = false, ftol = 1e-8)
+				srand(1987) #reseeding to x0 is same every time
+				x0 = ones(2*N-1) + randn(2*N-1)
+				#solution = nlsolve(wfocs!,x0,method=:trust_region, show_trace = true, ftol = 1e-8, extended_trace = true, iterations = 1000)
+				solution = nlsolve(wfocs!,wfocs_jac!,x0,method=:trust_region, show_trace = false, ftol = 1e-8, extended_trace = false, iterations = 1000)
 				est_rhos = solution.zero[1:N] # need to return this. These are the prices for the price schedule
 				est_lambdas = [lambda_lb; solution.zero[N+1:end]; lambda_ub] # remaining parameters are lambda cutoffs
 				
@@ -316,8 +339,7 @@ for market in markets
 			W = eye(2*N)
 			x0 = [17.0, 13.0, 0.0, 0.0, 6.0]
 			g(x) = obj_func(x,N,W)
-			#println(g(x0))
-			optim_res = optimize(g,x0,NelderMead(),OptimizationOptions(show_every = true, iterations = 1000))
+			optim_res = optimize(g,x0,SimulatedAnnealing(),OptimizationOptions(show_every = true, iterations = 1000))
 			println(optim_res)
 			min_X = Optim.minimizer(optim_res)
 			println(min_X)
